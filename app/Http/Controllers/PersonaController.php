b42088c3f23f270464909c500997d690552e7af5
Martín Lorenzo <mlorenzo290686@gmail.com>
2024-07-19 00:21:45 -0300

    ingresar controlador Persona

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Persona;
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller
+{
+    public function alta(Request $request)
+    {
+        $persona = new Persona();
+        $persona->nombre = $request->nombre;
+        $persona->apellido = $request->apellido;
+        $persona->telefono = $request->telefono;
+        $persona->save();
+        return response()->setStatusCode(201);
+    }
+    
+    
+}

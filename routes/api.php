945e1df65c1a80132794221515d937471d94e926
Martín Lorenzo <mlorenzo290686@gmail.com>
2024-07-20 16:52:41 -0300

    Ajustando Api.php

--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,12 @@
 use App\Http\Controllers\PersonaController;
 use Illuminate\Support\Facades\Route;
 
-<<<<<<< HEAD
-Route::prefix('persona')->group(function (){
+
+
   Route::post('/alta', [PersonaController::class, 'alta']); 
   Route::post('/baja', [PersonaController::class, 'baja']);
   Route::post('/modificacion', [PersonaController::class, 'modificacion']);
-
-});
-
-=======
-  Route::post('/alta', [PersonaController::class, 'alta']);
-  Route::delete('/baja', [PersonaController::class, 'baja']);
-  Route::put('/modificacion', [PersonaController::class, 'modificacion']);
   Route::get('/listado', [PersonaController::class, 'listado']);
   Route::get('/busqueda', [PersonaController::class, 'busqueda']);
+
 ;
->>>>>>> listado

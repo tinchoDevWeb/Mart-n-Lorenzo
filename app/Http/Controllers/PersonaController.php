852ec648e00de07a4726f4a71b91e5816b065970
Martín Lorenzo <mlorenzo290686@gmail.com>
2024-07-20 01:19:46 -0300

    ingresar baja

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -9,6 +9,7 @@ class PersonaController extends Controller
 {
     public function alta(Request $request)
     {
+
         $persona = new Persona();
         $persona->nombre = $request->nombre;
         $persona->apellido = $request->apellido;
@@ -17,5 +18,13 @@ class PersonaController extends Controller
         return response()->setStatusCode(201);
     }
     
+    public function baja(Request $request)
+    {
+        $persona = Persona::find($request->id);
+        $persona->delete();
+        return response()->setStatusCode(204);
+    }
+
     
+
 }

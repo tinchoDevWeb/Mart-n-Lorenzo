b3edfa2c9da5e489495789ef4a201da0f969ef72
Martín Lorenzo <mlorenzo290686@gmail.com>
2024-07-20 12:48:27 -0300

    Ingresar Modificación

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -18,5 +18,21 @@ class PersonaController extends Controller
         return response()->setStatusCode(201);
     }
     
-
+    public function baja(Request $request)
+    {
+        $persona = Persona::find($request->id);
+        $persona->delete();
+        return response()->setStatusCode(204);
+    }
+ 
+    public function modificacion(Request $request)
+    {
+        $persona = Persona::find($request->id);
+        $persona->nombre = $request->nombre;
+        $persona->apellido = $request->apellido;
+        $persona->telefono = $request->telefono;
+        $persona->save();
+        return response()->setStatusCode(200);
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PersonaController;
 use Illuminate\Support\Facades\Route;
 
-  Route::post('/alta', [PersonaController::class, 'alta']);  
-  
+  Route::post('/alta', [PersonaController::class, 'alta']);
+  Route::delete('/baja', [PersonaController::class, 'baja']);
+  Route::put('/modificacion', [PersonaController::class, 'modificacion']);
 ;
